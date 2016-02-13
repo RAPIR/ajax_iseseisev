55f1aa55a6c3dad451557c677f2ba301cb1b631e
Rajur <rajur.hint@gmail.com>
2016-02-13 18:41:06 +0200

    Registration Form Project

--- test.php
+++ test.php
@@ -1,5 +1,30 @@
 <?php
 
-echo $_GET['name'];
+$con = mysqli_connect("localhost","root","","test");
 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    $sel = "select * from users where email='$email'";
+
+    $run = mysqli_query($con,$sel);
+
+    $check_email = mysqli_num_rows($run);
+
+    if($check_email==1) {
+        echo "<h2>This emal is already registered, please try anohter!</h2>";
+        exit();
+    }
+        else {
+
+            $insert = "insert into users (name,pass,email) values('$name','$pass','$email')";
+            $run_insert = mysqli_query($con,$insert);
+
+                if($run_insert) {
+                    echo "<h2>Registration Successful, Thanks!</h2>";
+                }
+
+
+    }
 ?>
